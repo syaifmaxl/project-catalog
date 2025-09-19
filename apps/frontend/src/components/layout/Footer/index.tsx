8b04a1423910f8a180ed5e8389021d68235f76c2
mardin <tredingb25@gmail.com>
2025-09-19 09:44:47 +0700

    fix: hover background in icon youtube in footer.tsx

--- apps/frontend/src/components/layout/Footer/index.tsx
+++ apps/frontend/src/components/layout/Footer/index.tsx
@@ -57,7 +57,7 @@ const Footer = () => {
                     className="w-5 h-5 sm:w-6 sm:h-6 transition-all duration-300"
                   />
                 </div>
-                <div className="group relative p-2 sm:p-3 bg-white/10 backdrop-blur-sm rounded-lg sm:rounded-xl border border-white/10 hover:bgwhbg-white/10 transition-all duration-300 cursor-pointer hover:scale-110 hover:border-white/40">
+                <div className="group relative p-2 sm:p-3 bg-white/10 backdrop-blur-sm rounded-lg sm:rounded-xl border border-white/10 hover:bg-white/10 transition-all duration-300 cursor-pointer hover:scale-110 hover:border-white/40">
                   <img
                     src={youtube}
                     alt="YouTube"
